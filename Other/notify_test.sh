3f0303c95d71e52bc151c1b518b280ac938c46f1
Firstp1ck <al.leuzi@hotmail.com>
2025-06-02 14:12:30 +0200

    added notify test script

---
+++ Other/notify_test.sh
@@ -0,0 +1,8 @@
+# Low priority notification
+notify-send -u low "Low Priority" "This is a low priority test message"
+
+# Normal priority notification
+notify-send -u normal "Normal Priority" "This is a normal priority test message"
+
+# Critical priority notification
+notify-send -u critical "Critical Priority" "This is a critical priority test message"
