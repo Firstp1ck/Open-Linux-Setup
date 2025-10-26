85767907f36e5f7d36c6c2bd45718a6d93a5224c
Firstp1ck <al.leuzi@hotmail.com>
2025-10-26 14:58:20 +0100

    update aur monitor ro 1min

--- Other/Start_aur_monitor.sh
+++ Other/Start_aur_monitor.sh
@@ -4,7 +4,7 @@
 # sudo pacman -S libnotify gnu-netcat
 
 while ! nc -z -w3 aur.archlinux.org 22; do
-    date
-    sleep 30
+    echo "$(date) - AUR SSH is DOWN"
+    sleep 60
 done
 notify-send -u normal -i network-server -a port-watch "AUR SSH is UP" "aur.archlinux.org:22 is accepting connections"
